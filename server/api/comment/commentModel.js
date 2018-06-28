0df711795a8fdd71f2090d222674a2cee767b950
Adam <adamcampbelldev@gmail.com>
2018-06-28 16:50:52 +0100

    Updated text field on comment model and added validation.

--- server/api/comment/commentModel.js
+++ server/api/comment/commentModel.js
@@ -1,10 +1,35 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+function rawValidatorFunction(val) {
+    // more sophisticated validation to be implemented. For now it just checks shallow equality.
+    if (val === 'Deleted') {
+        return true;
+    }
+    const props = Object.getOwnPropertyNames(val);
+    console.log(props);
+    // confirm that no extra fields are present
+    if (props.length !== 2) {
+        return false;
+    }
+    // confirm that the object includes the fields 'blocks' and 'entityMap'
+    if (!props.includes('blocks') || !props.includes('entityMap')) {
+        return false;
+    }
+    // if all tests are passed then return true
+    return true;
+}
+
+const rawValidatorErrMsg = `
+    It looks like the data you supplied to the isn't valid. The text field should only ever be 
+    supplied with data exported from theDraftJS editor, and never with manually constructed data.`;
+
+const rawValidator = [rawValidatorFunction, rawValidatorErrMsg];
+
 const CommentSchema = new Schema({
     text: { 
-        //type: Schema.Types.Mixed,
-        type: String,
+        type: Schema.Types.Mixed,
+        validate: rawValidator,
         required: true
     },
 
@@ -15,12 +40,14 @@ const CommentSchema = new Schema({
 
     parents: [{
         type: Schema.Types.ObjectId,
-        ref: 'comment'
+        ref: 'comment',
+        required: true
     }],
 
     discussion: {
         type: Schema.Types.ObjectId,
-        ref: 'post'
+        ref: 'post',
+        required: true
     },
 
     createdAt: {
@@ -33,6 +60,7 @@ const CommentSchema = new Schema({
         default: Date.now
     }
 
-});
+},
+{minimize: false});
 
 module.exports = mongoose.model('comment', CommentSchema);
